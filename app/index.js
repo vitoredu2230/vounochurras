5276be649aaccf54fe4a72ea7212758e39520a9d
vitoredu2230 <vitoredu.wi@gmail.com>
2025-02-24 00:55:59 -0300

    Finalizado

--- app/index.js
+++ app/index.js
@@ -12,10 +12,12 @@ function changeLink() {
     let homeLink = document.getElementById("home");
     let createLink = document.getElementById("create");
     let listLink = document.getElementById("list");
+    let createUserLink = document.getElementById("create-user");
 
     homeLink.classList.remove("active");
     createLink.classList.remove("active");
     listLink.classList.remove("active");
+    createUserLink.classList.remove("active");
 
     if (currentPath.includes("/app/index.html")) {
         homeLink.classList.add("active");
@@ -23,5 +25,7 @@ function changeLink() {
         createLink.classList.add("active");
     } else if (currentPath.includes("/app/pages/churrascos/index.html")) {
         listLink.classList.add("active");
+    } else if (currentPath.includes("/app/pages/cadastro-usuarios/index.html")) {
+        createUserLink.classList.add("active");
     }
 };
